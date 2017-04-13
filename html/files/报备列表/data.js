b67d671914dfe10c85fac75f5b5dc04b469278bd
Jooper.github <junpeng.ge@sifude.com>
2017-04-13 17:58:09 +0800

    项目管理模块初版

---
+++ html/files/报备列表/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="报备列表.html",d="generationDate",e=new Date(1492077383009.97),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="rl_date",k="rl_day",l="page",m="packageId",n="97f79ba3f82a4709b5c9106d751bd7b3",o="type",p="Axure:Page",q="name",r="报备列表",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
---
+++ html/files/报备管理/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k],l,_(m,n,o,p,q,r,s,_(t,u),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[])),W,_(),X,_());}; 
+var b="url",c="报备管理.html",d="generationDate",e=new Date(1492077383005.96),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="rl_date",k="rl_day",l="page",m="packageId",n="80408afddfbf4192ab34235d603791df",o="type",p="Axure:Page",q="name",r="报备管理",s="notes",t="说明",u="<p><span>1.总代/开发商</span><span>有报备信息</span><span>的编辑/查看仅限于编辑报备信息的</span><span>处理</span><span>状态；分销</span><span>员工</span><span>有报备信息的查看/添加/修改权限；分销管理员有报备信息的所有权限；</span></p>",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
+return _creator();
+})());
