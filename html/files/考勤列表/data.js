d4484b035dbaeea04b8b7c2b8fdeadbdd62919b9
Jooper.github <junpeng.ge@sifude.com>
2017-07-06 18:11:32 +0800

    添加资讯管理模块

---
+++ html/files/考勤列表/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k],l,_(m,n,o,p,q,r,s,_(t,u),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[])),W,_(),X,_());}; 
+var b="url",c="考勤列表.html",d="generationDate",e=new Date(1499335706648.98),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="rl_date",k="rl_day",l="page",m="packageId",n="8e65d1c16cba45e7992eeb90d267a134",o="type",p="Axure:Page",q="name",r="考勤列表",s="notes",t="说明",u="<p><span style=\"font-family:'PingFang SC Regular', 'PingFang SC';\">拥有查看考勤权限的人员只能查看同权限组的员工的考勤记录</span></p>",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
+return _creator();
+})());
---
+++ html/files/考勤管理【一期不做】/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="考勤管理【一期不做】.html",d="generationDate",e=new Date(1499335706645.98),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="rl_date",k="rl_day",l="page",m="packageId",n="41f6f62cb0cc4ed78987d765e13e9f9d",o="type",p="Axure:Page",q="name",r="考勤管理【一期不做】",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
