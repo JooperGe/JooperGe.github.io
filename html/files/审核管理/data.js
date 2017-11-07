33bbe81bb125d20f28f8ff80e2a31ccae318a102
Jooper <junpeng.ge@sifude.com>
2017-11-07 17:04:31 +0800

    添加原型详细描述

---
+++ html/files/审核管理/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k],l,_(m,n,o,p,q,r,s,_(t,u),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[])),W,_(),X,_());}; 
+var b="url",c="审核管理.html",d="generationDate",e=new Date(1510044688892.91),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="rl_date",k="rl_day",l="page",m="packageId",n="21bf7d3fcc94457d902a04c8172985b6",o="type",p="Axure:Page",q="name",r="审核管理",s="notes",t="说明",u="<p><span>1.该页面</span><span>及其</span><span>子页面</span><span>仅限系统服务提供商有且有全部权限</span></p>",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
+return _creator();
+})());
